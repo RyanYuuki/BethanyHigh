568881dc79c1ffa5d3909527b5072ca42ad4d713
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-12-10 02:12:47 +0530

    feat: init provider

--- lib/theme/provider.dart
+++ lib/theme/provider.dart
@@ -1,47 +1,125 @@
 import 'package:flutter/material.dart';
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:hive/hive.dart';
+import 'package:school_app/data/constants.dart';
 import 'package:school_app/theme/themes.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  bool isLightMode = true;
-  ThemeData _selectedTheme;
-  Color? _seedColor;
+  bool isLightMode;
+  bool isOled;
+  late ThemeData _selectedTheme;
+  late String currentThemeMode;
+  Color _seedColor;
+  late double glowMultiplier;
+  late double radiusMultiplier;
+  late double blurMultiplier;
+  late int selectedVariantIndex;
 
-  ThemeProvider({Color? initialColor})
-      : _seedColor = initialColor ?? Colors.deepPurple,
-        _selectedTheme = AppTheme.darkTheme {
-    _loadDynamicTheme();
+  List<String> availThemeModes = ["default", "material", "custom"];
+
+  ThemeProvider()
+      : _seedColor = Colors.green,
+        isLightMode =
+            Hive.box("themeData").get("isLightMode", defaultValue: true),
+        isOled = Hive.box("themeData").get("isOled", defaultValue: false),
+        glowMultiplier =
+            Hive.box("themeData").get("glowMultiplier", defaultValue: 1.0),
+        radiusMultiplier =
+            Hive.box("themeData").get("radiusMultiplier", defaultValue: 1.0),
+        blurMultiplier =
+            Hive.box("themeData").get("blurMultiplier", defaultValue: 1.0),
+        selectedVariantIndex =
+            Hive.box("themeData").get("selectedVariantIndex", defaultValue: 0),
+        currentThemeMode =
+            Hive.box("themeData").get("themeMode", defaultValue: "default") {
+    _determineSeedColor();
+    _updateTheme();
   }
 
   ThemeData get selectedTheme => _selectedTheme;
 
-  Future<void> _loadDynamicTheme() async {
-    final corePalette = await DynamicColorPlugin.getCorePalette();
-    if (corePalette != null) {
-      _seedColor = Color(corePalette.primary.get(40));
-    } else {
+  void _determineSeedColor() {
+    if (currentThemeMode == "default") {
       _seedColor = Colors.green;
+    } else if (currentThemeMode == "material") {
+      loadDynamicTheme();
+    } else {
+      final box = Hive.box("themeData");
+      int colorIndex = box.get("customColorIndex", defaultValue: 0);
+      _seedColor = colorList[colorIndex];
     }
+  }
+
+  Future<void> loadDynamicTheme() async {
+    currentThemeMode = "material";
+    Hive.box("themeData").put("themeMode", "material");
+    final corePalette = await DynamicColorPlugin.getCorePalette();
+    _seedColor =
+        corePalette != null ? Color(corePalette.primary.get(40)) : Colors.green;
+    _updateTheme();
+  }
+
+  void updateGlowMultiplier(double value) {
+    Hive.box("themeData").put("glowMultiplier", value);
+    glowMultiplier = value;
+    notifyListeners();
+  }
+
+  void updateRadiusMultiplier(double value) {
+    Hive.box("themeData").put("radiusMultiplier", value);
+    radiusMultiplier = value;
+    notifyListeners();
+  }
+
+  void updateBlurMultiplier(double value) {
+    Hive.box("themeData").put("blurMultiplier", value);
+    blurMultiplier = value;
+    notifyListeners();
+  }
+
+  void updateSchemeVariant(int index) {
+    Hive.box("themeData").put("selectedVariantIndex", index);
+    selectedVariantIndex = index;
     _updateTheme();
   }
 
   void toggleTheme() {
     isLightMode = !isLightMode;
+    Hive.box("themeData").put("isLightMode", isLightMode);
     _updateTheme();
   }
 
   void setLightMode() {
     isLightMode = true;
+    Hive.box("themeData").put("isLightMode", true);
     _updateTheme();
   }
 
   void setDarkMode() {
     isLightMode = false;
+    Hive.box("themeData").put("isLightMode", false);
     _updateTheme();
   }
 
-  void setCustomSeedColor(Color newColor) {
-    _seedColor = newColor;
+  void setDefaultTheme() {
+    currentThemeMode = "default";
+    Hive.box("themeData").put("themeMode", "default");
+    _seedColor = Colors.green;
+    _updateTheme();
+  }
+
+  void setCustomSeedColor(int index) {
+    currentThemeMode = "custom";
+    Hive.box("themeData")
+      ..put("themeMode", "custom")
+      ..put("customColorIndex", index);
+    _seedColor = colorList[index];
+    _updateTheme();
+  }
+
+  void toggleOled(bool value) {
+    isOled = value;
+    Hive.box("themeData").put("isOled", value);
     _updateTheme();
   }
 
@@ -49,17 +127,27 @@ class ThemeProvider extends ChangeNotifier {
     _selectedTheme = isLightMode
         ? AppTheme.lightTheme.copyWith(
             colorScheme: ColorScheme.fromSeed(
-              seedColor: _seedColor!,
-              brightness: Brightness.light,
-            ),
+                seedColor: _seedColor,
+                brightness: Brightness.light,
+                dynamicSchemeVariant:
+                    dynamicSchemeVariantList[selectedVariantIndex]),
           )
-        : AppTheme.darkTheme.copyWith(
-            colorScheme: ColorScheme.fromSeed(
-              seedColor: _seedColor!,
-              brightness: Brightness.dark,
-            ),
-          );
-
+        : isOled
+            ? AppTheme.darkTheme.copyWith(
+                colorScheme: ColorScheme.fromSeed(
+                    seedColor: _seedColor,
+                    brightness: Brightness.dark,
+                    surface: Colors.black,
+                    dynamicSchemeVariant:
+                        dynamicSchemeVariantList[selectedVariantIndex]),
+              )
+            : AppTheme.darkTheme.copyWith(
+                colorScheme: ColorScheme.fromSeed(
+                    seedColor: _seedColor,
+                    brightness: Brightness.dark,
+                    dynamicSchemeVariant:
+                        dynamicSchemeVariantList[selectedVariantIndex]),
+              );
     notifyListeners();
   }
 }
