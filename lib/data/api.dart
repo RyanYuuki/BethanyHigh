bcacdcdd7c5191ef3492738a86a6a16dbfcc1284
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-12-10 02:08:42 +0530

    feat: made bethany scrapper

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -131,11 +131,12 @@ Future<dynamic> fetchHomePage() async {
       for (var el in policiesSelector) {
         var title = el.querySelector(".et_pb_text_inner h3")?.text;
         var description = '';
-        if (el.querySelector(".et_pb_text_inner p")?.text != null && el.querySelector(".et_pb_text_inner p")?.text != '') {
-          description =
-              el.querySelector(".et_pb_text_inner p")!.text;
+        if (policiesSelector.indexOf(el) == policiesSelector.length - 1) {
+          description = el.querySelectorAll(".et_pb_text_inner")[1].text;
+          log("true - $description");
         } else {
-            el.querySelector(".et_pb_text_inner")!.text;
+          description = el.querySelector(".et_pb_text_inner p")!.text;
+          log(description);
         }
 
         var imgUrl =
@@ -152,7 +153,7 @@ Future<dynamic> fetchHomePage() async {
         for (var source in sourceTags) {
           final index = sourceTags.indexOf(source);
           if (index == 0 || index == 4 || index == 8 || index == 12) {
-            departmentImages.add(source.attributes['srcset']);
+            departmentImages.add("https:${source.attributes['srcset']}");
           }
         }
       }
@@ -175,4 +176,270 @@ Future<dynamic> fetchHomePage() async {
   } catch (e) {
     log('Error: $e');
   }
+}
+
+Future<dynamic> fetchEvents() async {
+  try {
+    final dio = Dio();
+    final response =
+        await dio.get('https://bethanyinstitutions.edu.in/news-and-events/');
+
+    if (response.statusCode == 200) {
+      final document = parse(response.data);
+
+      final postElements = document.querySelectorAll('.et_pb_post');
+
+      List<Map<String, String>> posts = postElements.map((element) {
+        final titleElement = element.querySelector('.entry-title a');
+        final title = titleElement?.text.trim() ?? '';
+
+        final url = titleElement?.attributes['href'] ?? '';
+
+        final imageElement =
+            element.querySelector('.et_pb_image_container img');
+        final imageUrl = imageElement?.attributes['data-src'] ?? '';
+
+        final authorElement = element.querySelector('.author.vcard a');
+        final author = authorElement?.text.trim() ?? '';
+
+        final dateElement = element.querySelector('.post-meta .published');
+        final date = dateElement?.text.trim() ?? '';
+
+        final descriptionElement =
+            element.querySelector('.post-content-inner p');
+        final description = descriptionElement?.text.trim() ?? '';
+
+        return {
+          'title': title,
+          'link': url,
+          'image': imageUrl,
+          'author': author,
+          'date': date,
+          'description': description,
+        };
+      }).toList();
+
+      posts
+          .removeWhere((element) => element['title']!.contains("Top Achiever"));
+      log(posts.toString());
+      return posts;
+    } else {
+      log('Failed to load webpage: ${response.statusCode}');
+    }
+  } catch (e) {
+    log('Error: $e');
+  }
+}
+
+Future<dynamic> fetchEventData(String url) async {
+  Dio dio = Dio();
+  final resp = await dio.get(url);
+  if (resp.statusCode == 200) {
+    var document = parse(resp.data);
+    final title = document
+        .querySelector('.et_pb_text_0_tb_body .et_pb_text_inner')
+        ?.text
+        .trim();
+    final date = document
+        .querySelector('.et_pb_text_1_tb_body .et_pb_text_inner')
+        ?.text
+        .trim();
+    final categoryLink = document
+        .querySelector('.et_pb_text_2_tb_body .et_pb_text_inner a')
+        ?.attributes['href'];
+    final categoryName = document
+        .querySelector('.et_pb_text_2_tb_body .et_pb_text_inner a')
+        ?.text
+        .trim();
+    final institutionName = document
+        .querySelector('.et_pb_text_3_tb_body .et_pb_text_inner')
+        ?.text
+        .trim();
+
+    final storyElements = document.querySelectorAll(
+        '.et_pb_module.et_pb_post_content.et_pb_post_content_0_tb_body p span, i span, p');
+    var paragraphs = storyElements
+        .map((e) => e.text.trim())
+        .where((text) => text.isNotEmpty)
+        .where((text) => containsMostlyEnglishText(text))
+        .toList();
+    if (paragraphs.isEmpty ||
+        (paragraphs.length < 5 &&
+            paragraphs.any((el) => el.contains('Copyright')))) {
+      final elements = document.querySelectorAll(
+          '.et_pb_module.et_pb_post_content.et_pb_post_content_0_tb_body div');
+      paragraphs = elements
+          .map((el) => el.text.trim())
+          .where((text) => text.isNotEmpty)
+          .where((text) => containsMostlyEnglishText(text))
+          .toList();
+    }
+    final data = {
+      'title': title,
+      'date': date,
+      'image': document
+              .querySelector(
+                  ".et_pb_module.et_pb_image.et_pb_image_0_tb_body img")
+              ?.attributes['data-src'] ??
+          document
+              .querySelector(
+                  ".et_pb_module.et_pb_image.et_pb_image_0_tb_body img")
+              ?.attributes['src'],
+      'category': {'name': categoryName, 'link': categoryLink},
+      'institution': institutionName,
+      'body': paragraphs,
+    };
+    log(data.toString());
+    return data;
+  }
+}
+
+Future<dynamic> fetchAboutDataByUrl(String url) async {
+  Dio dio = Dio();
+  var resp = await dio.get(url);
+
+  if (resp.statusCode == 200) {
+    var document = parse(resp.data);
+    var body = document
+        .querySelectorAll(".et_pb_text_inner p")
+        .map((el) => el.text.trim())
+        .toList();
+    var image = document
+        .querySelector(".et_pb_module.et_pb_image.et_pb_image_0 img")!
+        .attributes['data-src'];
+
+    return {"body": body, "image": image};
+  }
+}
+
+Future<List<Map<String, String>>> fetchDepartments() async {
+  Dio dio = Dio();
+  Response response =
+      await dio.get("https://bethanyinstitutions.edu.in/contact-us/");
+
+  if (response.statusCode == 200) {
+    var document = parse(response.data);
+
+    final List<Map<String, String>> departments = [];
+
+    final Map<String, List<dynamic>> institutionMap = {
+      'Bethany High School, Koramangala': [
+        document.querySelector('.et_pb_text_3')!,
+        document.querySelector('.et_pb_blurb_0')!,
+        document.querySelector('.et_pb_blurb_1')!,
+        document.querySelector('.et_pb_blurb_2')!,
+      ],
+      'Bethany High School, Sarjapur': [
+        document.querySelector('.et_pb_text_4')!,
+        document.querySelector('.et_pb_blurb_3')!,
+        document.querySelector('.et_pb_blurb_4')!,
+        document.querySelector('.et_pb_blurb_5')!,
+      ],
+      'Bethany Professional Childcare': [
+        document.querySelector('.et_pb_text_5')!,
+        document.querySelector('.et_pb_blurb_6')!,
+        document.querySelector('.et_pb_blurb_7')!,
+        document.querySelector('.et_pb_blurb_8')!,
+      ],
+      'Bethany Pre-Primary Department': [
+        document.querySelector('.et_pb_text_6')!,
+        document.querySelector('.et_pb_blurb_9')!,
+        document.querySelector('.et_pb_blurb_10')!,
+        document.querySelector('.et_pb_blurb_11')!,
+      ],
+      'Bethany High-School Department': [
+        document.querySelector('.et_pb_text_7')!,
+        document.querySelector('.et_pb_blurb_12')!,
+        document.querySelector('.et_pb_blurb_13')!,
+        document.querySelector('.et_pb_blurb_14')!,
+      ],
+      'Bethany Middle-School Department': [
+        document.querySelector('.et_pb_text_8')!,
+        document.querySelector('.et_pb_blurb_15')!,
+        document.querySelector('.et_pb_blurb_16')!,
+        document.querySelector('.et_pb_blurb_17')!,
+      ],
+      'Bethany Junior College': [
+        document.querySelector('.et_pb_text_9')!,
+        document.querySelector('.et_pb_blurb_18')!,
+        document.querySelector('.et_pb_blurb_19')!,
+        document.querySelector('.et_pb_blurb_20')!,
+      ],
+      'Bethany Junior School 2 – HSR': [
+        document.querySelector('.et_pb_text_10')!,
+        document.querySelector('.et_pb_blurb_21')!,
+        document.querySelector('.et_pb_blurb_22')!,
+        document.querySelector('.et_pb_blurb_23')!,
+      ],
+      'Bethany Special School': [
+        document.querySelector('.et_pb_text_11')!,
+        document.querySelector('.et_pb_blurb_24')!,
+        document.querySelector('.et_pb_blurb_25')!,
+        document.querySelector('.et_pb_blurb_26')!,
+      ],
+      'Bethany Integrated Hostel': [
+        document.querySelector('.et_pb_text_12')!,
+        document.querySelector('.et_pb_blurb_27')!,
+        document.querySelector('.et_pb_blurb_28')!,
+        document.querySelector('.et_pb_blurb_29')!,
+      ],
+      'Bethany Primary Department': [
+        document.querySelector('.et_pb_text_13')!,
+        document.querySelector('.et_pb_blurb_30')!,
+        document.querySelector('.et_pb_blurb_31')!,
+        document.querySelector('.et_pb_blurb_32')!,
+      ],
+    };
+
+    institutionMap.forEach((name, elements) {
+      departments.add({
+        'name': name,
+        'location': elements[1]
+            .querySelector('.et_pb_blurb_description p')!
+            .text
+            .trim(),
+        'email': elements[2]
+            .querySelector('.et_pb_blurb_description p')!
+            .text
+            .trim(),
+        'phone': elements[3]
+            .querySelector('.et_pb_blurb_description p')!
+            .text
+            .trim(),
+      });
+    });
+
+    return departments;
+  } else {
+    throw Exception("Failed to load departments");
+  }
+}
+
+bool containsMostlyEnglishText(String text) {
+  if (text.startsWith('<') ||
+      text.contains('*') ||
+      text.contains('Website') ||
+      text.contains('Comment') ||
+      text.contains('Save my name,')) {
+    return false;
+  }
+  final alphaText = text.replaceAll(RegExp(r'[^a-zA-Z]'), '');
+  if (alphaText.isEmpty) return false;
+
+  final alphabeticRatio = alphaText.length / text.length;
+
+  return alphabeticRatio > 0.5;
+}
+
+class Data {
+  String heading1 = "Bethany High School Overview";
+  String description1 =
+      "Bethany High is an ICSE and ISC affiliated school located in the sixth block of Koramangala, Bangalore, India. It has over 2500 students from Nursery through Class 12. Bethany High has an ISC Junior College, of Classes 11 and 12. It also has a hostel (Bethany Integrated Home) for boarders and Bethany Special School for children with special needs, located in the fourth block of Koramangala. The school was founded by Mrs. Mignon David in 1963 and served as a hostel for children from Cottons and Baldwins. The Bethany colors are green and black, with similar-colored uniforms. The Bethany High motto is “Trust and Obey”. This phrase appears in both, its school song and school hymn. Bethany High School turned 50 in 2013 and opened up a new campus on Sarjapur Road, which was inaugurated by Mahatma Gandhi’s granddaughter Sumitra Kulkarni. In 2023, Bethany Institutions celebrated it’s Diamond Jubilee, 60 years of being a lighthouse of education in Bangalore.";
+  String image1 =
+      "https://bethanyinstitutions.edu.in/wp-content/uploads/2024/11/IMG-20240524-WA0006-scaled.jpg";
+  String heading2 = "System Of Education";
+  String description2 =
+      "Bethany High follows the ICSE curriculum up till Class 10. Students are offered a choice of subjects from Groups II and III. Students can choose any two of Mathematics, Science (Physics, Chemistry, Biology), Economics or Commercial Studies. From Group III, students can opt for any one subject out of Computer Applications, Art, Drama, Western Music or Physical Education. Students are also given a choice of Second Language from Hindi and Kannada. The academic year begins in June and lasts to March of the following year. For classes 8, 9 and 10 (of the high school), there are two Unit Tests (of 20 marks) and two Term Exams (of 80 marks each). The Class 10 students will appear for the ICSE Board Examinations as their 10th standard finals in March. For Classes 11 and 12 the school follows ISC. There are three streams a student can opt for; Science, Commerce or Arts (Humanities). The school offers subjects such as Physics, Chemistry, Biology, Computer Science, Mathematics, Commerce, Accounts, Business Studies, Psychology, Sociology, Fine Arts, Physical Education and Mass Media. The Class 12 students appear for the ISC Board Examinations in March.";
+  String image2 =
+      "https://bethanyinstitutions.edu.in///wp-content/uploads/2023/10/DSC_6862-768x511-1.webp";
 }
