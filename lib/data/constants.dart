5f8ec5b127849b525a121821626bef018631957c
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-12-10 02:09:00 +0530

    chore: declared constants for global use

---
+++ lib/data/constants.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+Map<String, Color> colorMap = {
+  "Green": Colors.green,
+  "Red": Colors.red,
+  "Pink": Colors.pink,
+  "Purple": Colors.purple,
+  "DeepPurple": Colors.deepPurple,
+  "Indigo": Colors.indigo,
+  "Blue": Colors.blue,
+  "LightBlue": Colors.lightBlue,
+  "Cyan": Colors.cyan,
+  "Teal": Colors.teal,
+  "LightGreen": Colors.lightGreen,
+  "Lime": Colors.lime,
+  "Yellow": Colors.yellow,
+  "Amber": Colors.amber,
+  "Orange": Colors.orange,
+  "DeepOrange": Colors.deepOrange,
+  "Brown": Colors.brown,
+};
+List<Color> colorList = colorMap.values.toList();
+List<String> colorKeys = colorMap.keys.toList();
+
+Map<String, DynamicSchemeVariant> dynamicSchemeVariantMap = {
+  for (var variant in DynamicSchemeVariant.values)
+    capitalize(variant.name): variant,
+};
+
+List<DynamicSchemeVariant> dynamicSchemeVariantList =
+    dynamicSchemeVariantMap.values.toList();
+List<String> dynamicSchemeVariantKeys = dynamicSchemeVariantMap.keys.toList();
+
+String capitalize(String word) {
+  String firstLetter = (word[0]).toUpperCase();
+  String truncatedWord = firstLetter + word.substring(1, word.length);
+  return truncatedWord;
+}
